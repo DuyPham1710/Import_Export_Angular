30a00e8d74d2f8aa7b31eda73256ede0ff3881cb
Pham Ngoc Duy <duysm17102004@gmail.com>
2025-09-17 10:36:14 +0700

    edit endpoint

--- src/core/constants/constant.ts
+++ src/core/constants/constant.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:5063/api';
+export const API_URL = 'https://import-export-api-caeyf5cdcdh4cjdj.eastasia-01.azurewebsites.net/api';
 export const PAGE_SIZE = 10;
